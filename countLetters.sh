69e483f3409b59e57b1e9029e8c402a91f69a151
ChristopherzwTan <christopher.tanzw@gmail.com>
2014-09-14 21:59:50 -0700

    Added the script that calculates letter frequency

---
+++ countLetters.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+grep A -o Cipher1.txt | wc -l
+grep B -o Cipher1.txt | wc -l
+grep C -o Cipher1.txt | wc -l
+grep D -o Cipher1.txt | wc -l
+grep E -o Cipher1.txt | wc -l
+grep F -o Cipher1.txt | wc -l
+grep G -o Cipher1.txt | wc -l
+grep H -o Cipher1.txt | wc -l
+grep I -o Cipher1.txt | wc -l
+grep J -o Cipher1.txt | wc -l
+grep K -o Cipher1.txt | wc -l
+grep L -o Cipher1.txt | wc -l
+grep M -o Cipher1.txt | wc -l
+grep N -o Cipher1.txt | wc -l
+grep O -o Cipher1.txt | wc -l
+grep P -o Cipher1.txt | wc -l
+grep Q -o Cipher1.txt | wc -l
+grep R -o Cipher1.txt | wc -l
+grep S -o Cipher1.txt | wc -l
+grep T -o Cipher1.txt | wc -l
+grep U -o Cipher1.txt | wc -l
+grep V -o Cipher1.txt | wc -l
+grep W -o Cipher1.txt | wc -l
+grep X -o Cipher1.txt | wc -l
+grep Y -o Cipher1.txt | wc -l
+grep Z -o Cipher1.txt | wc -l
