6d67be42d3620625a650ff903b4a7a75c5a003cf
ChristopherzwTan <christopher.tanzw@gmail.com>
2014-09-17 07:58:01 -0700

    Process of cracking part 2

---
+++ countCipher2.sh
@@ -0,0 +1,28 @@
+#! /bin/bash
+
+grep A -o Cipher2.txt | wc -l
+grep B -o Cipher2.txt | wc -l
+grep C -o Cipher2.txt | wc -l
+grep D -o Cipher2.txt | wc -l
+grep E -o Cipher2.txt | wc -l
+grep F -o Cipher2.txt | wc -l
+grep G -o Cipher2.txt | wc -l
+grep H -o Cipher2.txt | wc -l
+grep I -o Cipher2.txt | wc -l
+grep J -o Cipher2.txt | wc -l
+grep K -o Cipher2.txt | wc -l
+grep L -o Cipher2.txt | wc -l
+grep M -o Cipher2.txt | wc -l
+grep N -o Cipher2.txt | wc -l
+grep O -o Cipher2.txt | wc -l
+grep P -o Cipher2.txt | wc -l
+grep Q -o Cipher2.txt | wc -l
+grep R -o Cipher2.txt | wc -l
+grep S -o Cipher2.txt | wc -l
+grep T -o Cipher2.txt | wc -l
+grep U -o Cipher2.txt | wc -l
+grep V -o Cipher2.txt | wc -l
+grep W -o Cipher2.txt | wc -l
+grep X -o Cipher2.txt | wc -l
+grep Y -o Cipher2.txt | wc -l
+grep Z -o Cipher2.txt | wc -l
